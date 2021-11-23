9af3d5d3be061834cc5cc0eb58dbc93cbfd5260a
Twennnn <pa.popovish@gmail.com>
2021-11-24 00:47:39 +0700

    chore(constants): add id, public link

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,8 @@
+export const VK_BOT_ID = 'club208820464';
+
 export const YOUTUBE_CHANNEL_LINK = 'https://www.youtube.com/channel/UCiLEhVm2RKhjBJ4m2YmOtKA';
 export const VK_PUBLIC_LINK = 'https://vk.com/public180378356';
+export const VK_BOT_PUBLIC_LINK = `https://vk.com/${VK_BOT_ID}`;
 export const DISCORD_LINK = 'https://discord.gg/9nVkTAC';
 
 export const DEFAULT_COLOR = '#EA9ACC';
