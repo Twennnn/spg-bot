49e4ea9bf85e7d6ad4ab842517774f763a688f86
Twennnn <pa.popovish@gmail.com>
2022-05-21 22:53:13 +0700

    chore(index): new color command

---
+++ vk/commands/color/index.js
@@ -0,0 +1,45 @@
+import commonTags from 'common-tags';
+import { ButtonColor, Keyboard } from 'vk-io';
+
+import { Command } from '../command';
+
+import { hyperLink } from '../../../utils';
+
+const { stripIndents } = commonTags;
+
+export class Color extends Command {
+
+    constructor() {
+        super('color');
+    }
+
+    execute(context) {
+        context.send({
+            message: stripIndents`
+            🌈 ${hyperLink(`Цвет игрока ${context.member.nickname}: `)}
+            
+            На данный момент установлен: #AAAAA
+            `,
+            keyboard: Keyboard.builder()
+                .textButton({
+                    label: 'Изменить',
+                    color: ButtonColor.SECONDARY,
+                    payload: {
+                        command: 'set_color'
+                    }
+                })
+                .row()
+                .textButton({
+                    label: 'Отмена',
+                    color: ButtonColor.NEGATIVE,
+                    payload: {
+                        command: 'info'
+                    }
+                })
+                .row()
+                .inline()
+        });
+    }
+}
+
+export * from './setColor';
