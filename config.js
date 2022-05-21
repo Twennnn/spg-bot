282c86c05eab81c7bfba4319bcb212011f3b5df7
Twennnn <pa.popovish@gmail.com>
2022-05-21 23:03:27 +0700

    chore(config): export new consts

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
+import { Member, Blacklist } from './db';
+
 import _config from './config.json' assert { type: 'json' };
 
-export const config = _config;
+export const members = await Member.find();
+export const blacklist = await Blacklist.find();
+
+export const config = {
+    ..._config,
+    members: members.filter(({ probation }) => !probation),
+    probation_members: members.filter(({ probation }) => probation),
+    blacklist: blacklist
+};
