5d884771b100083d7a242dd8a489c2204decebe7
Twennnn <pa.popovish@gmail.com>
2022-05-21 22:42:32 +0700

    chore(constants): add new constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,17 @@
 export const VK_BOT_ID = 'club208820464';
+export const VK_CHAT_ID = 1;
+export const VK_CONVERSATION_ID_OFFSET = 2000000000;
 
 export const YOUTUBE_CHANNEL_LINK = 'https://www.youtube.com/channel/UCiLEhVm2RKhjBJ4m2YmOtKA';
 export const VK_PUBLIC_LINK = 'https://vk.com/public180378356';
 export const VK_BOT_PUBLIC_LINK = `https://vk.com/${VK_BOT_ID}`;
 export const DISCORD_LINK = 'https://discord.gg/9nVkTAC';
 
+export const MEETING_CHANNEL_ID = '533713111757094933';
+
+export const MAYOR_ROLE_ID = '503205127503478805';
+export const MEMBERS_ROLE_ID = '503205741298188289';
+export const PROBATION_MEMBERS_ROLE_ID = '596795380189102082';
+
 export const DEFAULT_COLOR = '#EA9ACC';
+
