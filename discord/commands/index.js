8b139c367bb7b3672aec4b37dd74c812033b9f14
Twennnn <pa.popovish@gmail.com>
2022-05-21 22:51:43 +0700

    chore(index): add new command

--- discord/commands/index.js
+++ discord/commands/index.js
@@ -3,10 +3,15 @@ import { client } from '../client';
 import { Hello } from './hello';
 import { Info } from './info';
 import { Member } from './member';
+import { Alert } from './alert';
 
-const commands = [
-    Hello, Info, Member
-];
+const commands =
+    [
+        Hello,
+        Info,
+        Member,
+        Alert
+    ];
 
 const applicationCommands = commands.map((Command) => {
    const command = new Command()
