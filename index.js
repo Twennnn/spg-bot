fabe39115de5140f1f76a387e296889f137398fd
Twennnn <pa.popovish@gmail.com>
2022-05-21 23:03:00 +0700

    chore(index): connect mongoose

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-import './discord';
-import './vk';
+mongoose.connect(`mongodb+srv://twennnn:${process.env.MONGODB_PASSWORD}@cluster0.kwssd.mongodb.net/SPG_Bot?retryWrites=true&w=majority`)
+.then(async () => {
+    console.log('----MongoDB был успешно запущен----');
+
+    import('./discord');
+    import('./vk');
+})
+.catch((error) => {
+    console.error('Ошибка при подключении к MongoDB:');
+    console.error(error)
+})
