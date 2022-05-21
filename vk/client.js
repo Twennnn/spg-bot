4e6e78d7b9dbcaa99775e99975ee1f5929ba0814
Twennnn <pa.popovish@gmail.com>
2022-05-21 23:00:53 +0700

    chore(client): add new imports and export consts

--- vk/client.js
+++ vk/client.js
@@ -1,8 +1,12 @@
 import { VK } from 'vk-io';
 import { HearManager } from '@vk-io/hear';
+import { SessionManager } from '@vk-io/session';
+import { SceneManager } from '@vk-io/scenes';
 
 export const client = new VK({
     token: process.env.VK_TOKEN
 });
 
 export const hearManager = new HearManager();
+export const sessionManager = new SessionManager();
+export const sceneManager = new SceneManager();
