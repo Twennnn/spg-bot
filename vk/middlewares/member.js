84d5746f0d696ff0049f0b685f5b13872ef9e886
Twennnn <pa.popovish@gmail.com>
2022-05-24 22:19:27 +0700

    chore(command): add verification cheker

--- vk/middlewares/member.js
+++ vk/middlewares/member.js
@@ -1,11 +1,23 @@
 import { client } from '../client';
-import { Member } from '../../db';
+import { createUser, Member } from '../../db';
 
 client.updates.on('message_new', async (context, next) => {
-    const member = await Member.findOne({ 'vkId': `${context.senderId}` });
-    if (!member) {
-        return context.send()
+        const member = await Member.findOne({ 'vkId': `${context.senderId}` });
+        if (member) {
+            context.member = member
+        } else {
+            const member = await createUser(context.senderId)
+                .catch(() => null);
+
+            if (member) {
+                context.member = member;
+            } else {
+                await context.send('🙄 Произошла ошибка при регистрации аккаунта, попробуйте еще раз!')
+
+                return;
+            }
+        }
+
+        next();
     }
-    context.member = member;
-    next();
-})
+)
