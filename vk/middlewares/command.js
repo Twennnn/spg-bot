b80339659ee90d8fd2d117a36ea57fbc7bddd370
Twennnn <pa.popovish@gmail.com>
2022-05-24 22:18:57 +0700

    chore(command): add condition

--- vk/middlewares/command.js
+++ vk/middlewares/command.js
@@ -1,6 +1,9 @@
 import { client } from '../client';
 
 client.updates.on('message_new', (context, next) => {
+    if (context.isChat) {
+        return
+    }
     const { messagePayload } = context;
 
     context.state.command = messagePayload?.command;
