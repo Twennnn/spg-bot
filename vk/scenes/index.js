c2a09907772272fd30d78aeb0d42705052fa1e20
Twennnn <pa.popovish@gmail.com>
2022-05-27 22:23:05 +0700

    chore(scenes): add new imports

--- vk/scenes/index.js
+++ vk/scenes/index.js
@@ -1,6 +1,10 @@
 import './addMember';
 import './deleteMember';
+import './editMember';
+
 import './addToBlacklist';
 import './deleteFromBlacklist';
+
 import './setDescription';
+
 import './setColor';
