62e97bf61b11254dbc20cd0a93c847a9fbf13299
Twennnn <pa.popovish@gmail.com>
2022-05-27 22:21:59 +0700

    chore(deleteFromBlacklist): moved context.send

--- vk/scenes/deleteFromBlacklist.js
+++ vk/scenes/deleteFromBlacklist.js
@@ -5,7 +5,6 @@ import { StepScene } from '@vk-io/scenes';
 import { sceneManager } from '../client';
 import { blacklist } from '../../config';
 import { chunkArray } from '../../utils';
-import { hyperLink } from '../utils';
 
 sceneManager.addScenes([
     new StepScene('delete_from_blacklist', {
@@ -23,13 +22,13 @@ sceneManager.addScenes([
                             })
                                 .row();
                         });
-                            keyboard.textButton({
-                                label: 'Отмена',
-                                color: ButtonColor.NEGATIVE,
-                                payload: {
-                                    command: 'help'
-                                }
-                            })
+                        keyboard.textButton({
+                            label: 'Отмена',
+                            color: ButtonColor.NEGATIVE,
+                            payload: {
+                                command: 'help'
+                            }
+                        })
 
                         await context.send({
                             message: isFirstPage ?
@@ -48,13 +47,6 @@ sceneManager.addScenes([
                 }
 
                 context.scene.state.nickname = context.text;
-
-                return context.scene.step.next();
-            },
-            async (context) => {
-                const { nickname } = context.scene.state;
-
-                await context.send(`👤 Игрок ${hyperLink(nickname)} успешно удален из черного списка`)
 
                 return context.scene.step.next();
             }
