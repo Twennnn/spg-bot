f824a08ec4aba34a45913d3e9b282bea2955d65c
Twennnn <pa.popovish@gmail.com>
2022-05-29 23:53:59 +0700

    chore(member): change permission and rewrote message

--- vk/commands/member/index.js
+++ vk/commands/member/index.js
@@ -2,19 +2,25 @@ import commonTags from 'common-tags';
 import { ButtonColor, Keyboard } from 'vk-io';
 
 import { Command } from '../command';
+import { Member as Memberr } from '../../../db';
+import { hyperLink } from '../../utils';
 
 const { stripIndents } = commonTags;
 
 export class Member extends Command {
 
     constructor() {
-        super('member', 10);
+        super('member');
     }
 
     async execute(context) {
+        const countAllMembers = await Memberr.find({ probation: { $exists: true }}).count();
+
         context.send({
             message: stripIndents`
-            🔎 Выбор действий для работы с людьми. Нажмите на одну из кнопок на клавиатуре!
+            🔎 Жителей в базе данных на данный момент: ${hyperLink(countAllMembers)}
+            
+            Выбор действий для работы с людьми. Нажмите на одну из кнопок на клавиатуре!
             `,
             keyboard: Keyboard.builder()
                 .textButton({
