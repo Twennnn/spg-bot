95258fef81445f1c156c7ff575331686b750f9cf
Twennnn <pa.popovish@gmail.com>
2022-07-01 23:31:21 +0700

    chore(emoji): add emoji

--- utils/emoji.js
+++ utils/emoji.js
@@ -1,13 +1,14 @@
 export const YOUTUBE = '<:YouTube:908816711610925096>';
 export const VK = '<:client:909466450144665610>';
-export const SPOTIFY = '<:Spotify:916006016783630376> ';
+export const SPOTIFY = '<:Spotify:916006016783630376>';
 export const STEAM = '<:steam:916389921344847912>';
-export const DISCORD = '<:discord:919513927992348712> ';
+export const DISCORD = '<:discord:919513927992348712>';
+export const GITHUB = '<:github:991814942636380170>'
 
 export const GOLDENAPPLE = '<:goldenapple:916389819725267004>';
 export const COOKIE = '<:cookie:909460810609344553>';
 export const BARRIER = '<:barrier:909844993861120030>';
-export const BOOKNQUILL = '<:booknquill:909846571443707975> ';
-export const BREAD = '<:bread:909460786705997854> ';
-export const EMERALD = '<:emerald:909460989966188594> ';
-export const XPBOTTLE = '<:xpbottle:909460835607384114> ';
+export const BOOKNQUILL = '<:booknquill:909846571443707975>';
+export const BREAD = '<:bread:909460786705997854>';
+export const EMERALD = '<:emerald:909460989966188594>';
+export const XPBOTTLE = '<:xpbottle:909460835607384114>';
