a97cfc4cc76c74ab5133ef8c157e32ee14d214ed
Twennnn <pa.popovish@gmail.com>
2022-07-01 23:29:40 +0700

    chore(getCurrentNickname): bug fix

--- utils/getCurrentNickname.js
+++ utils/getCurrentNickname.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export function getCurrentNickname(nickname) {
+    if (!nickname) {
+        return;
+    }
     return axios.get(`https://api.ashcon.app/mojang/v2/user/${nickname}`)
         .then(({ data: { username } }) => {
             return username
