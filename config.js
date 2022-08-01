ea3331b177c9f08bddbcd884d8b040473677dea6
Twennnn <pa.popovish@gmail.com>
2022-08-02 01:51:51 +0700

    chore(config): delete const

--- config.js
+++ config.js
@@ -1,17 +1,3 @@
-import { Member, Blacklist } from './db';
-
 import _config from './config.json' assert { type: 'json' };
 
-export const members = await Member.find();
-export const blacklist = await Blacklist.find();
-
-export const config = {
-    ..._config,
-    members: members.filter(({ probation, permission }) => (
-        probation === false && permission > 1
-    )),
-    probation_members: members.filter(({ probation, permission }) =>  (
-        probation === true && permission > 1
-    )),
-    blacklist: blacklist
-};
+export const config = _config;
