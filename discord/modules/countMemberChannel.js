a4999c8ee3472650ab293fe8331f259de8546282
Twennnn <pa.popovish@gmail.com>
2022-08-02 01:59:19 +0700

    chore(countMembersChannel): fix

--- discord/modules/countMemberChannel.js
+++ discord/modules/countMemberChannel.js
@@ -1,11 +1,11 @@
 import { client } from '../client';
 import { COUNT_MEMBER_DISCORD_CHANNEL_ID, DISCORD_ID } from '../../utils';
-import { countMembers } from '../utils';
+import { countAllMembers } from '../utils';
 
 client.on('ready', () => {
     const guild = client.guilds.cache.get(DISCORD_ID)
     setInterval(async () => {
         const channel = guild.channels.cache.get(COUNT_MEMBER_DISCORD_CHANNEL_ID)
-        await channel.setName(`Жителей в городе: ${await countMembers()}`)
+        await channel.setName(`Жителей в городе: ${await countAllMembers()}`)
     }, 5 * 60 * 1_000);
 })
