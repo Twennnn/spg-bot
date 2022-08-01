b74f3e7a0d108016594bf23b971afff40297103c
Twennnn <pa.popovish@gmail.com>
2022-08-02 01:51:06 +0700

    chore(index): code fix

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 mongoose.connect(`mongodb+srv://twennnn:${process.env.MONGODB_PASSWORD}@cluster0.kwssd.mongodb.net/SPG_Bot?retryWrites=true&w=majority`)
-.then(async () => {
-    console.log('----MongoDB был успешно запущен----');
+    .then(async () => {
+        console.log('----MongoDB был успешно запущен----');
 
-    import('./discord');
-    import('./vk');
-})
-.catch((error) => {
-    console.error('Ошибка при подключении к MongoDB:');
-    console.error(error)
-})
+        import('./discord');
+        import('./vk');
+    })
+    .catch((error) => {
+        console.error('Ошибка при подключении к MongoDB:');
+        console.error(error)
+    });
